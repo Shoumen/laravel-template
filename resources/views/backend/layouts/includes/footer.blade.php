3115f7e46b2f35dd59c3cbede9b7fa145e57dd34
Shoumen <shoumen853@gmail.com>
2025-05-28 14:06:21 +0600

    d

--- resources/views/backend/layouts/includes/footer.blade.php
+++ resources/views/backend/layouts/includes/footer.blade.php
@@ -1,5 +1,5 @@
 <footer id="footer" class="footer">
     <div class="copyright">
-        &copy; Copyright <strong><span>NiceAdmin</span></strong>. All Rights Reserved
+        &copy; Copyright <strong><span>Himadrita</span></strong>. All Rights Reserved
     </div>
 </footer>
